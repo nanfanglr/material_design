74c62e653f0d8b1c15df1a3e6d39f9cbbfc04c04
liurui <1513093228@qq.com>
2018-08-22 17:29:08 +0800

    添加部分测试系统栏代码

--- app/src/main/java/com/rui/material_design/MainActivity.java
+++ app/src/main/java/com/rui/material_design/MainActivity.java
@@ -1,10 +1,15 @@
 package com.rui.material_design;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,12 +21,25 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     @BindView(R.id.button2)
     Button button2;
+    @BindView(R.id.head_bar)
+    TextView headBar;
+    @BindView(R.id.root_layout)
+    ConstraintLayout rootLayout;
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        BarUtils.setStatusBarColor(this, ContextCompat.getColor(this, R.color.colorPrimary), 0);
+        BarUtils.addMarginTopEqualStatusBarHeight(rootLayout);
+
+//        BarUtils.setStatusBarVisibility(this, false);
+//        BarUtils.setNavBarVisibility(this, false);
+//        BarUtils.setNavBarImmersive(this.getWindow());
+
+        BarUtils.setNavBarColor(this, Color.parseColor("#55000000"));
     }
 
     @OnClick({R.id.button, R.id.button2})
